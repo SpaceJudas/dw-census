762f8ddab55dc627bceef24fd181b75760c1cbae
Spencer Ewall <spencerjewall@gmail.com>
2016-10-17 20:01:14 -0700

    prettiefied main

--- Main.py
+++ Main.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
+'''
+me at the console
+
+b = Book()
+b.open()
+'''
 
 '''
 This is all mine!
@@ -7,21 +13,32 @@ Requirements:
  - https://github.com/watson-developer-cloud/python-sdk
 
 '''
+import config
 
-# Open the book with the given name
-
-
-def openBook(bookName):
-  return open('./data/{}.txt'.format(bookName), 'r')
-
-# Cleans an array of lines from a file. This trims whitespace and removes
-# empty lines.
+import os
 
 
-def cleanLines(lines):
-  moddedLines = filter(lambda str: len(
-      str) > 0, map(lambda str: str.strip(), lines))
+class Book:
+  # Open the book with the given name
+  def __init__(self, name='Wyrd Sisters', fileExtension='txt'):
+    self._filename = os.path.join('./data','{}.{}'.format(name, fileExtension))
+
+  #returns a file object
+  def open(self):
+    return open(self._filename, 'r')
+
+### Utils
+# Cleans an array of lines from a file. This trims whitespace and removes empty lines.
+def cleanTextArray(arr):
+  moddedLines = filter(lambda str: len(str) > 0,
+                  map(lambda str: str.strip(), lines))
   return list(moddedLines)
 
-book = openBook('Wyrd Sisters')
-lines = cleanLines(book.readlines())
+
+### main stuff
+#from Main import Book
+#b = Book()
+#bookText = b.open().read()
+
+#import AlchemyApiAnalysis
+#print(AlchemyApiAnalysis.callEntityExtract(bookText))
